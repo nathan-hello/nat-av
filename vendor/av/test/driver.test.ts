d6d02a875abbf96a9eb74831495ff6ab764eb917
Nathan <nathanhello321@gmail.com>
2026-06-15 10:41:39 -0400

    moved debugger files

--- vendor/av/test/driver.test.ts
+++ vendor/av/test/driver.test.ts
@@ -1,5 +1,4 @@
 import { Manager } from "@av/drivers";
-import { Debugger } from "@av/drivers/builtin/debugger";
 import { RPCRequest } from "@av/rpc/protocol";
 import { DeviceRpcRouter } from "@av/rpc/server/device";
 import assert from "node:assert/strict";
@@ -90,38 +89,4 @@ describe("test driver", () => {
     assert.equal(peer2.sent.length, 2);
   });
 
-  it("preloads the debugger tree and appends messages to each node", () => {
-    const eventDriver = new EventDriver("event-1");
-    const natav = new Manager({
-      drivers: [eventDriver],
-      deferred: [Debugger],
-    });
-
-    const debuggerDriver = natav.GetDriver("debugger");
-
-    assert.ok(debuggerDriver);
-    assert.equal(debuggerDriver?.state.tree["event-1"]?.meta.name, "event-1");
-    assert.deepEqual(debuggerDriver?.state.tree["event-1"]?.messages, []);
-
-    natav.bus.dispatch("natav:debug:socket", {
-      name: "event-1",
-      data: {
-        traceName: "event-1",
-        direction: "rx",
-        time: 1,
-        encoding: "utf8",
-        data: new Uint8Array([1, 2, 3]),
-      },
-    });
-
-    assert.deepEqual(debuggerDriver?.state.tree["event-1"]?.messages, [
-      {
-        traceName: "event-1",
-        direction: "rx",
-        time: 1,
-        encoding: "utf8",
-        data: [1, 2, 3],
-      },
-    ]);
-  });
 });
